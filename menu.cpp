e2bd62d49251ea0071eb747d56c4c79997800378
Auxz <hayden56786@gmail.com>
2024-06-24 01:40:12 -0400

    Changes

--- menu.cpp
+++ menu.cpp
@@ -1,7 +1,3 @@
-#include <cstring>
-#include <iostream>
-#include <vector>
-#include <windows.h>
 #define NAVIGATION_UP "\x1b[A"
 #define NAVIGATION_DOWN VK_DOWN
 #define NAVIGATION_LEFT VK_LEFT
@@ -12,175 +8,135 @@
 #define ENTER "\n"
 #define ESC "\x1b"
 #define CSI "\x1b["
+#include "menu.hpp"
 
-namespace ax
+Option::Option(const int a_id, std::string a_name, const std::string a_description, const bool a_showDesc,
+               bool const a_showId)
 {
-namespace menu
+    m_id = a_id;
+    m_name = a_name;
+    m_description = a_description;
+    m_showDesc = a_showDesc;
+    m_showId = a_showId;
+    m_selected = false;
+    m_row = 0;
+}
+Option::~Option()
 {
-
-class Option
+}
+bool Option::isSelected()
 {
-  public:
-    int m_id;
-    int m_row;
-    std::string m_name;
-    std::string m_description;
-    bool m_showDesc;
-    bool m_showId;
+    return m_selected;
+}
+void Option::updateSelected(const bool a_selected)
+{
+    m_selected = a_selected;
+}
 
-  private:
-    bool m_selected;
+void Option::displayInformation()
+{
+    std::string optionString;
+    if (m_showId == true)
+    {
+        // optionString += std::to_string(m_id) + ". ";
+    }
+    optionString += m_name + " : ";
+    if (m_showDesc == true)
+    {
+        optionString += m_description;
+    }
+    std::cout << optionString << std::endl;
+}
 
-  public:
-    Option(const int a_id, std::string a_name, const std::string a_description, const bool a_showDesc,
-           bool const a_showId)
-    {
-        m_id = a_id;
-        m_name = a_name;
-        m_description = a_description;
-        m_showDesc = a_showDesc;
-        m_showId = a_showId;
-        m_selected = false;
-    }
-    ~Option()
-    {
-    }
-    bool isSelected()
-    {
-        return m_selected;
-    }
-    void updateSelected(const bool a_selected)
-    {
-        m_selected = a_selected;
-    }
+BaseMenu::BaseMenu(const std::string a_name)
+{
+    m_name = a_name;
+    isActive = false;
+}
+BaseMenu::BaseMenu(const std::string a_name,
+                   const std::vector<Option *>
+                       a_options) // Overloaded constructor to accept a pointer to a vector of Option object pointers
+{
+    m_name = a_name;
+    m_options = a_options;
+    isActive = false;
+}
+void BaseMenu::display()
+{
+}
 
-    void displayInformation()
+void BaseMenu::selectOption(int a_row)
+{
+    for (Option *option : m_options)
     {
-        std::string optionString;
-        if (m_showId == true)
+        if (option->m_row == a_row)
         {
-            optionString += std::to_string(m_id) + ". ";
+            option->updateSelected(true);
         }
-        optionString += m_name + " : ";
-        if (m_showDesc == true)
-        {
-            optionString += m_description;
-        }
-        std::cout << optionString << std::endl;
-    };
-};
-class BaseMenu
+    }
+}
+Option *BaseMenu::getInput()
 {
-  protected:
-    std::string m_name;
-    std::vector<Option *> m_options;
-    bool isActive;
-    BaseMenu(const std::string a_name)
-    {
-        m_name = a_name;
-        isActive = false;
-    }
-    BaseMenu(const std::string a_name,
-             const std::vector<Option *>
-                 a_options) // Overloaded constructor to accept a pointer to a vector of Option object pointers
-    {
-        m_name = a_name;
-        m_options = a_options;
-        isActive = false;
-    }
-    virtual void display()
-    {
-    }
-
-  public:
-    void selectOption(int a_row)
+    while (true)
     {
         for (Option *option : m_options)
         {
-            if (option->m_row == a_row)
+            if (option->isSelected())
             {
-                option->updateSelected(true);
+                return option;
             }
         }
     }
-    Option *getInput()
-    {
-        while (true)
-        {
-            for (Option *option : m_options)
-            {
-                if (option->isSelected())
-                {
-                    return option;
-                }
-            }
-        }
-    }
-};
-struct MenuHandler
+}
+
+MenuHandler::MenuHandler()
 {
-  private:
-    BaseMenu *m_activeMenu = nullptr;
+}
 
-  public:
-    MenuHandler()
-    {
-    }
-
-    void setActiveMenu(BaseMenu *a_menu)
-    {
-        m_activeMenu = a_menu;
-    }
-    BaseMenu *getActiveMenu()
-    {
-        return m_activeMenu;
-    }
-
-} menuHandler;
-class OptionsMenu : BaseMenu
+void MenuHandler::setActiveMenu(BaseMenu *a_menu)
 {
-  private:
-    MenuHandler *m_menuHandler;
-
-  public:
-    OptionsMenu(const std::string a_name) : BaseMenu(a_name)
-    {
-        m_menuHandler = &menuHandler;
-    }
-
-    OptionsMenu(const std::string a_name, const std::vector<Option *> a_options) : BaseMenu(a_name, a_options)
-    {
-    }
-    virtual void display(bool displayMenuName = false)
-    {
-        if (displayMenuName)
-        {
-            std::cout << m_name << std::endl;
-        }
-        for (Option *option : m_options)
-        {
-            std::cout << option->m_name << std::endl;
-            option->displayInformation();
-        }
-        m_menuHandler->setActiveMenu(this);
-    }
-    void addOption(Option *option)
-    {
-    }
-    Option addOption(const int a_id, const std::string a_name, const std::string a_desc, const bool a_showDesc = false,
-                     const bool a_showId = false)
-    {
-        Option newOption = Option(a_id, a_name, a_desc, a_showDesc, a_showId);
-        m_options.push_back(&newOption);
-        return newOption;
-    }
-};
-
-}; // namespace menu
-namespace userinput
+    m_activeMenu = a_menu;
+}
+BaseMenu *MenuHandler::getActiveMenu()
 {
+    return m_activeMenu;
+}
 
-void SendToInputHandler(char *inputBuffer[], const HANDLE *inputHandle, const HANDLE *outputHandle)
+OptionsMenu::OptionsMenu(const std::string a_name) : BaseMenu(a_name)
+{
+    // m_menuHandler = a_handler;
+}
+
+OptionsMenu::OptionsMenu(const std::string a_name, const std::vector<Option *> a_options) : BaseMenu(a_name, a_options)
+{
+    // m_menuHandler = a_handler;
+}
+void OptionsMenu::display(bool displayMenuName)
+{
+    if (displayMenuName)
+    {
+        std::cout << m_name << std::endl;
+    }
+    for (Option *option : m_options)
+    {
+        std::cout << option->m_name << std::endl;
+        option->displayInformation();
+    }
+    m_menuHandler->setActiveMenu(this);
+}
+void OptionsMenu::addOption(Option *option)
+{
+}
+Option OptionsMenu::addOption(const int a_id, const std::string a_name, const std::string a_desc, const bool a_showDesc,
+                              const bool a_showId)
+{
+    Option newOption = Option(a_id, a_name, a_desc, a_showDesc, a_showId);
+    m_options.push_back(&newOption);
+    return newOption;
+}
+// namespace menu
+
+void SendToInputHandler(char *inputBuffer[8], HANDLE *inputHandle, HANDLE *outputHandle)
 {
 
     if (strcmp(*inputBuffer, NAVIGATION_UP) == 0)
@@ -193,9 +149,8 @@ void SendToInputHandler(char *inputBuffer[], const HANDLE *inputHandle, const HA
 
         WriteConsole(outputHandle, CSI "6n", sizeof(*inputBuffer), NULL, NULL);
         char pBuffer[6];
-        ReadConsole(inputHandle, &pBuffer, 6, NULL);
-        menu::BaseMenu *activeMenu = menu::menuHandler.getActiveMenu();
-        activeMenu->selectOption((int)pBuffer[4]);
+        DWORD numberOfCharsRead = 0;
+        ReadConsole(inputHandle, &pBuffer, 6, &numberOfCharsRead, NULL);
     }
 }
 void WindowsConsole()
@@ -209,14 +164,14 @@ void WindowsConsole()
     char inputBuffer[8];
     DWORD numberOfChars = 8;
     DWORD readChars;
+    char *ptr = inputBuffer;
 
     while (true)
     {
         ReadConsole(hIn, &inputBuffer, numberOfChars, &readChars, NULL);
-        SendToInputHandler(&inputBuffer, &hOut);
+        std::cout << inputBuffer;
+        SendToInputHandler(&ptr, &hIn, &hOut);
     }
 }
 
-}; // namespace userinput
-
-}; // namespace ax
+// namespace ax
